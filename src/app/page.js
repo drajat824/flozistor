cf3fab5c799372a3f3311b6198fb2f1b52698808
drajat824 <drajat.fikri24@gmail.com>
2025-05-11 06:47:51 +0700

    Refactor PostPage content: reintroduce link to "Dari Pythagoras Sampai Sepotong Kue" for improved navigation

--- src/app/page.js
+++ src/app/page.js
@@ -25,16 +25,16 @@ export default function Home() {
 
           {/* Postingan */}
 
-          <Link href="/post/dari-pythagoras-sampai-sepotong-kue" onClick={() => window.location.href = "/post/dari-pythagoras-sampai-sepotong-kue"} style={{ cursor: 'pointer', textDecoration: 'none', color: 'black' }}>
-            <p style={{ fontWeight: 'bolder', fontSize: '2.3rem' }}>Dari Pythagoras Sampai Sepotong Kue</p>
-            <p style={{ fontSize: '1.3rem' }}>Sebuah kutipan dari Ki Hadjar Dewantara ini membuka tulisan saya kali ini..</p>
-            <p style={{ fontSize: '1rem' }}>Diposting oleh Drajat Fikri pada 1/3/2024 | Pendidikan</p>
-          </Link>
-          <hr style={{ border: '1px solid black', margin: '40px 0' }} />
           <Link href="/post/ai-dalam-pendidikan" onClick={() => window.location.href = "/post/ai-dalam-pendidikan"} style={{ cursor: 'pointer', textDecoration: 'none', color: 'black' }}>
             <p style={{ fontWeight: 'bolder', fontSize: '2.3rem' }}>AI dalam Pendidikan, Ancaman atau Kawan?</p>
             <p style={{ fontSize: '1.3rem' }}>Sebelum memulai pembahasan, mari kita bermain permainan sederhana berikut ini..</p>
             <p style={{ fontSize: '1rem' }}>Diposting oleh Drajat Fikri pada 10/5/2025 | Pendidikan</p>
+          </Link>
+          <hr style={{ border: '1px solid black', margin: '40px 0' }} />
+          <Link href="/post/dari-pythagoras-sampai-sepotong-kue" onClick={() => window.location.href = "/post/dari-pythagoras-sampai-sepotong-kue"} style={{ cursor: 'pointer', textDecoration: 'none', color: 'black' }}>
+            <p style={{ fontWeight: 'bolder', fontSize: '2.3rem' }}>Dari Pythagoras Sampai Sepotong Kue</p>
+            <p style={{ fontSize: '1.3rem' }}>Sebuah kutipan dari Ki Hadjar Dewantara ini membuka tulisan saya kali ini..</p>
+            <p style={{ fontSize: '1rem' }}>Diposting oleh Drajat Fikri pada 1/3/2024 | Pendidikan</p>
           </Link>
 
           {/* Button Postingan */}
@@ -50,16 +50,17 @@ export default function Home() {
           {/* Postingan */}
 
           {/* Repeatable Link Component */}
-          <Link href="/post/dari-pythagoras-sampai-sepotong-kue" onClick={() => window.location.href = "/post/dari-pythagoras-sampai-sepotong-kue"} style={{ cursor: 'pointer', textDecoration: 'none', color: 'black' }}>
-            <p style={{ fontWeight: 'bolder', fontSize: 30 }}>Dari Pythagoras Sampai Sepotong Kue</p>
-            <p style={{ fontSize: 20 }}>Sebuah kutipan dari Ki Hadjar Dewantara ini membuka tulisan saya kali ini..</p>
-            <p style={{ fontSize: 15 }}>Diposting oleh Drajat Fikri pada 1/3/2024 | Pendidikan</p>
-          </Link>
-          <hr style={{ border: '0.5px solid black', margin: '40px 0' }} />
+
           <Link href="/post/ai-dalam-pendidikan" onClick={() => window.location.href = "/post/ai-dalam-pendidikan"} style={{ cursor: 'pointer', textDecoration: 'none', color: 'black' }}>
             <p style={{ fontWeight: 'bolder', fontSize: 30 }}>AI dalam Pendidikan, Ancaman atau Kawan?</p>
             <p style={{ fontSize: 20 }}>Sebelum memulai pembahasan, mari kita bermain permainan sederhana berikut ini..</p>
             <p style={{ fontSize: 15 }}>Diposting oleh Drajat Fikri pada 10/5/2025 | Pendidikan</p>
+          </Link>
+          <hr style={{ border: '0.5px solid black', margin: '40px 0' }} />
+          <Link href="/post/dari-pythagoras-sampai-sepotong-kue" onClick={() => window.location.href = "/post/dari-pythagoras-sampai-sepotong-kue"} style={{ cursor: 'pointer', textDecoration: 'none', color: 'black' }}>
+            <p style={{ fontWeight: 'bolder', fontSize: 30 }}>Dari Pythagoras Sampai Sepotong Kue</p>
+            <p style={{ fontSize: 20 }}>Sebuah kutipan dari Ki Hadjar Dewantara ini membuka tulisan saya kali ini..</p>
+            <p style={{ fontSize: 15 }}>Diposting oleh Drajat Fikri pada 1/3/2024 | Pendidikan</p>
           </Link>
 
           {/* Button Postingan */}
